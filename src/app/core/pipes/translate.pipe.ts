7c0eeb49a0b3ef1d88885470e7b0e851a5cbda30
Sandeshth148 <sandeshth582@gmail.com>
2025-11-23 13:33:34 +0530

    Fix TranslatePipe to subscribe to language changes for dynamic updates

--- src/app/core/pipes/translate.pipe.ts
+++ src/app/core/pipes/translate.pipe.ts
@@ -1,15 +1,32 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { TranslationService } from '../services/translation.service';
+import { Subscription } from 'rxjs';
 
 @Pipe({
   name: 'translate',
   standalone: true,
   pure: false // Impure to update on language change
 })
-export class TranslatePipe implements PipeTransform {
-  constructor(private translationService: TranslationService) {}
+export class TranslatePipe implements PipeTransform, OnDestroy {
+  private subscription?: Subscription;
+
+  constructor(
+    private translationService: TranslationService,
+    private cdr: ChangeDetectorRef
+  ) {
+    // Subscribe to language changes to trigger pipe re-evaluation
+    this.subscription = this.translationService.currentLanguage.subscribe(() => {
+      this.cdr.markForCheck();
+    });
+  }
 
   transform(key: string): string {
     return this.translationService.translate(key);
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
